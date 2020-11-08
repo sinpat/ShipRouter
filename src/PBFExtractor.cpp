8c5729abaa490018a95569b47a8ed0872fafd4bc
lukas <np-hardass@protonmail.com>
2020-11-09 00:56:40 +0100

    print the "natural" tag

--- src/PBFExtractor.cpp
+++ src/PBFExtractor.cpp
@@ -1,7 +1,8 @@
-#include <string_view>
 #include <PBFExtractor.hpp>
-#include <osmpbfreader.h>
 #include <fmt/core.h>
+#include <osmpbfreader.h>
+#include <string_view>
+#include <fmt/ranges.h>
 
 
 
@@ -9,14 +10,26 @@ namespace {
 
 struct Visitor
 {
-    void node_callback(uint64_t osmid, double lon, double lat, const CanalTP::Tags& tags)
+    void node_callback(uint64_t osmid,
+                       double lon,
+                       double lat,
+                       const CanalTP::Tags& tags)
     {
-        fmt::print("{}\n", osmid);
+	  if(auto iter = tags.find("natural");
+		 iter != std::end(tags)) {
+            fmt::print("{}: {}\n",
+					   osmid,
+					   *iter);
+        }
     }
 
-    void way_callback(uint64_t osmid, const CanalTP::Tags& tags, const std::vector<uint64_t>& refs) {}
+    void way_callback(uint64_t osmid,
+                      const CanalTP::Tags& tags,
+                      const std::vector<uint64_t>& refs) {}
 
-    void relation_callback(uint64_t osmid, const CanalTP::Tags& tags, const CanalTP::References& refs) {}
+    void relation_callback(uint64_t osmid,
+                           const CanalTP::Tags& tags,
+                           const CanalTP::References& refs) {}
 };
 } // namespace
 
@@ -26,9 +39,5 @@ auto parsePBFFile(std::string_view path)
     -> void
 {
     Visitor v;
-	CanalTP::read_osm_pbf(path.data(), v);
+    CanalTP::read_osm_pbf(path.data(), v);
 }
-
-
-
-
