e088ff97d31422652e8a7e29d7503d3088caba70
lukas <np-hardass@protonmail.com>
2020-11-28 16:30:49 +0100

    added CoastlineLookup

---
+++ include/CoastlineLookup.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Coastline.hpp>
+#include <Utils.hpp>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class CoastlineLookup
+{
+public:
+    auto addCoastline(std::uint64_t osmid,
+                      Tags&& tags,
+                      std::vector<std::uint64_t>&& refs) noexcept
+        -> void;
+
+    auto getCoastline(std::uint64_t osmid) const noexcept
+        -> std::optional<CRef<Coastline>>;
+
+    auto deleteCoastline(std::uint64_t osmid) noexcept
+        -> void;
+
+private:
+    std::unordered_map<std::uint64_t, Coastline> coastlines_;
+};
---
+++ src/CoastlineLookup.cpp
@@ -0,0 +1,31 @@
+#include <Coastline.hpp>
+#include <CoastlineLookup.hpp>
+#include <Utils.hpp>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+auto CoastlineLookup::addCoastline(std::uint64_t osmid,
+                                   Tags&& tags,
+                                   std::vector<std::uint64_t>&& refs) noexcept
+    -> void
+{
+    coastlines_.try_emplace(osmid, std::move(tags), std::move(refs));
+}
+
+auto CoastlineLookup::getCoastline(std::uint64_t osmid) const noexcept
+    -> std::optional<CRef<Coastline>>
+{
+    auto iter = coastlines_.find(osmid);
+    if(iter == std::end(coastlines_)) {
+        return std::nullopt;
+    }
+
+    return std::cref(iter->second);
+}
+
+auto CoastlineLookup::deleteCoastline(std::uint64_t osmid) noexcept
+    -> void
+{
+    coastlines_.erase(osmid);
+}
