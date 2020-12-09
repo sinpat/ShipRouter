e77fe971182d728f5559ebe1a931b6a16654fe1f
lukas <np-hardass@protonmail.com>
2020-12-09 14:34:08 +0100

    added lat and lng classes

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -9,3 +9,32 @@ template<class T>
 using CRef = std::reference_wrapper<const T>;
 
 using Tags = std::unordered_map<std::string, std::string>;
+
+
+struct Lat
+{
+    explicit Lat(double value)
+        : value(value) {}
+
+    Lat() = default;
+    Lat(Lat&&) = default;
+    Lat(const Lat&) = default;
+    auto operator=(Lat&&) -> Lat& = default;
+    auto operator=(const Lat&) -> Lat& = default;
+
+    double value;
+};
+
+struct Lng
+{
+    explicit Lng(double value)
+        : value(value) {}
+
+    Lng() = default;
+    Lng(Lng&&) = default;
+    Lng(const Lng&) = default;
+    auto operator=(Lng&&) -> Lng& = default;
+    auto operator=(const Lng&) -> Lng& = default;
+
+    double value;
+};
