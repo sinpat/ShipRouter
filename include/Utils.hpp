7c886feb55c503f4beaae4ca44e901e6336d2a0b
lukas <np-hardass@protonmail.com>
2020-12-10 00:39:11 +0100

    adde concat function to concat all kind of stuff

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -10,3 +10,20 @@ template<class T>
 using CRef = std::reference_wrapper<const T>;
 
 using Tags = std::unordered_map<std::string, std::string>;
+
+
+template<class Head0, class Head1, class... Tail>
+constexpr auto concat(Head0&& head0, Head1&& head1, Tail&&... tail) noexcept
+{
+    if constexpr(sizeof...(tail) == 0) {
+        head0.insert(std::end(head0),
+                     std::begin(head1),
+                     std::end(head1));
+        return std::forward<Head0>(head0);
+    } else {
+        return concat(
+            concat(std::forward<Head0>(head0),
+                   std::forward<Head1>(head1)),
+            std::forward<Tail>(tail)...);
+    }
+}
