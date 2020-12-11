43cc67e1b3fa3e6d219d938e56c7883f8f7795e3
lukas <np-hardass@protonmail.com>
2020-12-11 13:02:44 +0100

    start the server the right way

--- src/main.cpp
+++ src/main.cpp
@@ -3,65 +3,62 @@
 #include <ServiceManager.hpp>
 #include <SphericalGrid.hpp>
 #include <Vector3D.hpp>
+#include <csignal>
+#include <cstdint>
 #include <execution>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
+static std::condition_variable condition;
+static std::mutex mutex;
+
+constexpr static inline auto PORT = 9999;
+
+static auto handleUserInterrupt(int signal)
+    -> void
+{
+    if(signal == SIGINT) {
+        condition.notify_one();
+    }
+}
+
+static auto waitForUserInterrupt()
+    -> void
+{
+    std::unique_lock lock{mutex};
+    condition.wait(lock);
+    fmt::print("user has signaled to interrupt the program...");
+    lock.unlock();
+}
+
 auto main() -> int
 {
+    std::signal(SIGINT, handleUserInterrupt);
+    std::signal(SIGPIPE, [](int /**/) {});
+
     auto [nodes, coastlines] = parsePBFFile("../data/antarctica-latest.osm.pbf");
     fmt::print("starting calculation of polygons...\n");
     auto polygons = calculatePolygons(std::move(coastlines),
                                       std::move(nodes));
-    // fmt::print("{}\n", polygons.size());
-    fmt::print("should be water: {}\n",
-               std::count_if(std::execution::par,
-                             std::begin(polygons),
-                             std::end(polygons),
-                             [](const auto& poly) {
-                                 return poly.pointInPolygon(Latitude<Degree>{-60},
-                                                            Longitude<Degree>{-80}); // is in land
-                             }));
-    fmt::print("should be water: {}\n",
-               std::count_if(std::execution::par,
-                             std::begin(polygons),
-                             std::end(polygons),
-                             [](const auto& poly) {
-                                 return poly.pointInPolygon(Latitude<Degree>{-70.15096965227654},
-                                                            Longitude<Degree>{-1.8873336911201477}); // is in water
-                             }));
-
-    fmt::print("should be land: {}\n",
-               std::count_if(std::execution::par,
-                             std::begin(polygons),
-                             std::end(polygons),
-                             [](const auto& poly) {
-                                 return poly.pointInPolygon(Latitude<Degree>{-70.58638474216802},
-                                                            Longitude<Degree>{-9.03076171875}); // is in water
-                             }));
-
-    fmt::print("should be land: {}\n",
-               std::count_if(std::execution::par,
-                             std::begin(polygons),
-                             std::end(polygons),
-                             [](const auto& poly) {
-                                 return poly.pointInPolygon(Latitude<Degree>{-64.19442343702701},
-                                                            Longitude<Degree>{-57.81005859375}); // is in water
-                             }));
-
     SphericalGrid grid{1000};
     grid.filter(polygons);
 
-    const auto& lats = grid.getLats();
-    const auto& lngs = grid.getLngs();
+    ServiceManager manager{Pistache::Address{Pistache::IP::any(),
+                                             PORT},
+                           grid};
+    try {
+        fmt::print("started server, listening at: {}",
+                   PORT);
 
-    Dijkstra dijk{grid};
+        manager.serveThreaded();
 
-    auto [path, distance] = dijk.findRoute(100, 150).value();
+        waitForUserInterrupt();
 
-    fmt::print("Route: {}\n", fmt::join(path, ","));
-    for(auto id : path) {
-        fmt::print("[{},{}],\n", lngs[id].getValue(), lats[id].getValue());
+        manager.shutdown();
+
+        fmt::print("shutting down server");
+
+    } catch(const std::exception& e) {
+        fmt::print("catched exception: {}", e.what());
     }
-    ServiceManager{Pistache::Address{"localhost", Pistache::Port{8000}}, grid};
 }
