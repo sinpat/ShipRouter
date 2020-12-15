8f73b8f98ba844768bed2ac5e7efac838a4c99c4
Patrick Singer <sinpat224@hotmail.com>
2020-12-15 22:56:59 +0100

    Fix neighbors in graph

--- backend/src/Graph.cpp
+++ backend/src/Graph.cpp
@@ -105,19 +105,6 @@ auto Graph::getNeigboursOf(NodeId node) const noexcept
     return nonstd::span{start, end};
 }
 
-auto Graph::sphericalToGrid(Latitude<Radian> theta,
-                            Longitude<Radian> phi) const noexcept
-    -> std::pair<size_t, size_t>
-{
-    fmt::print("lat: {}, lng: {}\n", theta.getValue(), phi.getValue());
-    auto pair = grid_.sphericalToGrid(theta, phi);
-
-    fmt::print("m: {}, n: {}\n", pair.first, pair.second);
-
-    return pair;
-}
-
-
 auto Graph::gridToId(std::size_t m, std::size_t n) const noexcept
     -> NodeId
 {
@@ -141,7 +128,7 @@ auto Graph::getRowGridNeigboursOf(std::size_t m, std::size_t n) const noexcept
                    std::back_inserter(ids),
                    [&](auto pair) {
                        auto [m, n] = pair;
-                       return gridToId(m, m);
+                       return gridToId(m, n);
                    });
 
     return ids;
@@ -157,7 +144,7 @@ auto Graph::getLowerGridNeigboursOf(std::size_t m, std::size_t n) const noexcept
                    std::back_inserter(ids),
                    [&](auto pair) {
                        auto [m, n] = pair;
-                       return gridToId(m, m);
+                       return gridToId(m, n);
                    });
 
     return ids;
@@ -173,7 +160,7 @@ auto Graph::getUpperGridNeigboursOf(std::size_t m, std::size_t n) const noexcept
                    std::back_inserter(ids),
                    [&](auto pair) {
                        auto [m, n] = pair;
-                       return gridToId(m, m);
+                       return gridToId(m, n);
                    });
 
     return ids;
@@ -191,9 +178,7 @@ auto Graph::getSnapNodeCandidate(Latitude<Degree> lat,
                                  Longitude<Degree> lng) const noexcept
     -> NodeId
 {
-    const auto [m, n] = sphericalToGrid(lat.toRadian(), lng.toRadian());
-
-    fmt::print("m: {}, n:{}\n", m, n);
+    const auto [m, n] = grid_.sphericalToGrid(lat.toRadian(), lng.toRadian());
 
     const auto id = gridToId(m, n);
 
