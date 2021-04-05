b2959e64896545b4cdd0eaa969b079070504a200
Patrick Singer <sinpat224@hotmail.com>
2021-04-05 21:08:35 +0200

    start of ch dijkstra

--- backend/include/CHDijkstra.hpp
+++ backend/include/CHDijkstra.hpp
@@ -1,6 +1,35 @@
 #pragma once
 
 #include <Graph.hpp>
+#include <queue>
+
+using Direction = uint;
+
+struct QNode
+{
+    QNode(NodeId node_in, Distance dist_in, Direction dir_in)
+        : node(node_in),
+          dist(dist_in),
+          dir(dir_in) {}
+
+    NodeId node;
+    Distance dist;
+    Direction dir; // forward or backward
+};
+
+struct CHDijkstraQueueComparer
+{
+    auto operator()(const QNode& lhs,
+                    const QNode& rhs) const noexcept
+        -> bool
+    {
+        return lhs.dist > rhs.dist;
+    }
+};
+
+using CHDijkstraQueue = std::priority_queue<QNode,
+                                            std::vector<QNode>,
+                                            CHDijkstraQueueComparer>;
 
 class CHDijkstra
 {
@@ -8,4 +37,18 @@ public:
     CHDijkstra(const Graph& graph) noexcept;
 
     DijkstraPath findShortestPath(NodeId source, NodeId target) noexcept;
+
+private:
+    std::pair<Path, Distance> unfoldPath(NodeId node);
+    void reset() noexcept;
+
+private:
+    const Graph& graph_;
+
+    CHDijkstraQueue q_;
+    std::vector<Distance> forward_dists_;
+    std::vector<Distance> backward_dists_;
+    std::vector<NodeId> forward_previous_;
+    std::vector<NodeId> backward_previous_;
+    std::vector<NodeId> touched_;
 };
--- backend/src/CHDijkstra.cpp
+++ backend/src/CHDijkstra.cpp
@@ -1,8 +1,57 @@
 #include <CHDijkstra.hpp>
 
-CHDijkstra::CHDijkstra(const Graph& graph) noexcept {}
+CHDijkstra::CHDijkstra(const Graph& graph) noexcept
+    : graph_(graph),
+      forward_dists_(graph_.size(), UNREACHABLE),
+      backward_dists_(graph_.size(), UNREACHABLE),
+      forward_previous_(graph_.size(), NON_EXISTENT),
+      backward_previous_(graph_.size(), NON_EXISTENT)
+{}
 
 DijkstraPath CHDijkstra::findShortestPath(NodeId source, NodeId target) noexcept
 {
+    reset(); // TODO: remove this and try to optimize
+    q_.emplace(source, 0, 0);
+    q_.emplace(target, 0, 1);
+
+    while(!q_.empty()) {
+        QNode q_node = q_.top();
+        NodeId cur_node = q_node.node;
+        q_.pop();
+
+        auto edges = graph_.relaxEdges(cur_node); // TODO: need to be filtered by level
+        for(auto edge : edges) {
+            NodeId target = edge.target;
+            Distance combined_dist = q_node.dist + edge.dist;
+            // TODO: handle forward and backward properly
+            if(combined_dist < forward_dists_[target]) {
+                forward_dists_[target] = combined_dist;
+                forward_previous_[target] = cur_node;
+                touched_.emplace_back(target);
+                q_.emplace(target, combined_dist, q_node.dir);
+            }
+        }
+    }
     return std::nullopt;
+}
+
+std::pair<Path, Distance> CHDijkstra::unfoldPath(NodeId node)
+{
+    Distance dist = forward_dists_[node] + backward_dists_[node];
+    // TODO: unfold
+    return std::pair{
+        std::vector<NodeId>{},
+        dist};
+}
+
+void CHDijkstra::reset() noexcept
+{
+    q_ = CHDijkstraQueue{};
+    for(auto id : touched_) {
+        forward_dists_[id] = UNREACHABLE;
+        backward_dists_[id] = UNREACHABLE;
+        forward_previous_[id] = NON_EXISTENT;
+        backward_previous_[id] = NON_EXISTENT;
+    }
+    touched_.clear();
 }
